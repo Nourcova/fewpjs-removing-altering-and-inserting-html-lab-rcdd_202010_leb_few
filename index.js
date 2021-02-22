75045f8529762414157b9c697fcd48a3ed7748c4
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-02-22 13:03:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove('#main');
+let h1=document.createElement('h1');
+h1.id="victory";
+document.body.appendChild(h1);
+let newHeader=document.createElement('newHeader');
+h1.appendChild(newHeader);
